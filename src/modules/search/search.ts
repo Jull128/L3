7e07a4b3cbd8f28c67d6abe26814550a0e4b0fd3
Jull128 <88882567+Jull128@users.noreply.github.com>
2023-10-28 18:12:35 +0500

    add search

---
+++ src/modules/search/search.ts
@@ -0,0 +1,21 @@
+// import { Component } from '../component';
+// import { Product } from '../product/product';
+// import html from './checkout.tpl.html';
+// import { formatPrice } from '../../utils/helpers';
+// import { cartService } from '../../services/cart.service';
+// import { ProductData } from 'types';
+// import { sendEvent } from '../../index';
+
+// class Search extends Component {
+//   products!: ProductData[];
+
+//   async render() {
+
+//   }
+
+//   private async _makeOrder() {
+
+//   }
+// }
+
+// export const searchComp = new Search(html);
