65c67178b3f99890baf07418c7444ae87f9a2829
Jull128 <88882567+Jull128@users.noreply.github.com>
2023-10-29 23:01:20 +0500

    global window

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,7 +2,11 @@ import localforage from 'localforage';
 import { genUUID } from '../utils/helpers';
 
 const ID_DB = '__wb-userId';
-
+declare global {
+  interface Window {
+    userId: string;
+  }
+}
 class UserService {
   async init() {
     const id = await this.getId();
